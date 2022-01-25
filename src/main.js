a6370a587ee55a98523182b9fa435dca8237f32c
lqs <2677388021@qq.com>
2022-01-25 13:45:36 +0800

    完成了用户面板（不含分配角色按钮）

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
-
+// https://lianghj.top:8888/api/private/v1/
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+})
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
 
